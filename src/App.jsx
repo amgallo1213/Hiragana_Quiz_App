55c61890d216f6ae28812d3976fcffa0e7c68506
Andrea <90292543+amgallo1213@users.noreply.github.com>
2023-01-24 23:05:37 -0500

    adding error message and form

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
 
     if (input.toLowerCase() === hiragana[current].romanji) {
       setStreak(streak + 1)
-      setMaxStreak(Math.max(streak, maxStreak))
+      setMaxStreak(Math.max(streak + 1, maxStreak))
       setError(false)
 
       localStorage.setItem('maxStreak', Math.max(streak, maxStreak))
@@ -91,19 +91,34 @@ function App() {
 
   useEffect(() => {
     setRandomHiragana()
-    setStreak(localStorage.getItem('streak') || 0)
-    setMaxStreak(localStorage.getItem('maxStreak') || 10)
+    setStreak(parseInt(localStorage.getItem('streak')) || 0)
+    setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0)
   }, [])
 
   return (
     <div className="min-h-screen bg-blue-800 text-white text-center">
       <header className="p-6 mb-8">
-      <h1 className='text-2xl font-bold uppercase'>Hiragana Quiz</h1>
-      <div>
-        <p> { streak }/{ maxStreak} </p>
-      </div>
+        <h1 className='text-2xl font-bold uppercase'>Hiragana Quiz</h1>
+        <div>
+          <p> { streak }/{ maxStreak} </p>
+        </div>
       </header>
+
+      <div className='text-9xl font-bold mb-8'>
+        { hiragana[current].hiragana }
+      </div>
+
+      <div className='mb-8'>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={input} 
+            onChange={handleChange}
+            className='block w-24 mx-auto pb-2 bg-transparent border-b-2 border-b-white outline-none text-center text-6xl'/>
+        </form>
+      </div>
       
+      {error && <p className='text-red-500 text-center'>{ error }</p>}
 
     </div>
   )
